adcd6f510ca3ef19e9a8bc3a03f6df09554f7bbc
Chinmay <rajchinmay08@gmail.com>
2023-07-26 23:23:18 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,16 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://user-auth-csv.netlify.app/",
+    credentials: true,
+  })
+);
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
@@ -15,9 +25,19 @@ mongoose.connect(process.env.MONGO_URI)
     });
   });
 
-// app.use(cors());
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://user-auth-csv.netlify.app/"
+  );
+  res.header(
+    "Access-Control-Allow-Origin",
+    "Origin,X-Requested-With,Content-Type,Accept",
+    "Access-Control-Allow-Methods: GET, DELETE, PUT, PATCH, HEAD, OPTIONS, POST"
+  );
+  next();
+});
 
 app.use(userRoutes);
 
