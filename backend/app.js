7df400414c946d9a29133ea09368d79cdd6fb363
Chinmay <rajchinmay08@gmail.com>
2023-07-26 19:36:39 +0530

    msg

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO_URI)
       console.log(`connected to db and listening on port ${process.env.PORT}`);
     });
   });
+
+app.use(cors());
 
 app.use(express.json());
 
