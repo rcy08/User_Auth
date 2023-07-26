fa856df80cc593ec62cba0bdcc69a330d1f32ac9
Chinmay <rajchinmay08@gmail.com>
2023-07-26 21:01:41 +0530

    dgsdfghsf

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -20,7 +20,7 @@ const Signup = () => {
 		e.preventDefault();
 		
 		if (password === confirmpassword && password !== '') {
-			const response = await fetch('https://main--shimmering-tartufo-8477cb.netlify.app/signup', {
+			const response = await fetch('https://user-auth-1mjc.onrender.com/signup', {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify({ 
