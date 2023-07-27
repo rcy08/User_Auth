a8eb3688598cf9ed3812a2a53c4d2ca00eeeb181
Chinmay <rajchinmay08@gmail.com>
2023-07-27 11:50:00 +0530

    adding OAuth2 client

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,14 +1,28 @@
+const { google } = require('googleapis');
 const nodemailer = require('nodemailer');
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const refresh_token = process.env.REFRESH_TOKEN;
+const redirect_uri = process.env.REDIRECT_URI;
 
-const sendEmail = (options) => {
+const oauth2client = new google.auth.OAuth2(client_id, client_secret, redirect_uri);
+oauth2client.setCredentials({ refresh_token });
+
+const sendEmail = async (options) => {
+    const accessToken = await oauth2client.getAccessToken();
+
     const transporter = nodemailer.createTransport({
         service: process.env.EMAIL_SERVICE,
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
+            type: 'OAuth2',
             user: process.env.EMAIL_USERNAME,
-            pass: process.env.PASSWORD
+            clientId: client_id,
+            clientSecret: client_secret,
+            refreshToken: refresh_token,
+            accessToken: accessToken,
         }
     });
 
